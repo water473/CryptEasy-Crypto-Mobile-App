f74fe2d24654e6ce59c43f2cc644cdd37475519b
unknown <dimitriosrko114@gmail.com>
2024-05-15 01:13:02 -0400

    Added an example fitted btc chart on HTML String. Index.html asset not properly working

--- lib/menu.dart
+++ lib/menu.dart
@@ -4,22 +4,27 @@ import 'package:webview_flutter/webview_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 
 const String kExamplePage = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Load file or HTML string example</title>
-</head>
-<body>
-
-<h1>Local demo page</h1>
-<p>
- This is an example page used to demonstrate how to load a local file or HTML
- string using the <a href="https://pub.dev/packages/webview_flutter">Flutter
- webview</a> plugin.
-</p>
-
-</body>
-</html>
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
+  {
+  "width": "500",
+  "height": "500",
+  "symbol": "BITSTAMP:BTCUSD",
+  "interval": "15",
+  "timezone": "America/New_York",
+  "theme": "dark",
+  "style": "0",
+  "locale": "en",
+  "allow_symbol_change": true,
+  "calendar": false,
+  "support_host": "https://www.tradingview.com"
+}
+  </script>
+</div>
+<!-- TradingView Widget END -->
 ''';
 
 enum _MenuOptions {
